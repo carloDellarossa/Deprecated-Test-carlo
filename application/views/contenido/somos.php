41e9f5e7ab352062fa2075aff9c2d7d89effe8a2
carloDellarossa <dellarossa-p@hotmail.com>
2017-05-10 19:09:12 -0300

    10-05-2017

--- application/views/contenido/somos.php
+++ application/views/contenido/somos.php
@@ -1,11 +1,38 @@
-¿Quiénes Somos?
-Librería Giorgio es una empresa dedicada a la venta de suministros de oficina, artículos escolares, juguetería, y mucho más... Contamos con instalaciones para la venta de nuestros productos por volumen a empresas, ofreciendo un servicio de despacho completamente gratis. Asímismo disponemos de salas de ventas para atención de público en general, con estacionamientos propios para nuestros clientes. Durante años nos hemos dedicado a ser especialistas en Escolares, ofreciendo todo lo necesario para la Lista Escolar con los mejores precios del mercado. Trabajamos cada día para atender las necesidades y demandas de nuestros clientes, ampliando nuestro stock de productos y marcas asociadas para lograr satisfacción y fidelidad.
+<style>
+  .Somos{
+    padding-top: 100px;
+    font-size: 16px;
+  }
 
-Nuestra historia
-Librería Giorgio es fundada el 1º de Septiembre del año 1992 en la ciudad de Concepción, inicialmente como un centro de fotocopiado ubicado en calle O`Higgins 734 local 14, Concepción. Considerando las necesidades de suministros e insumos para los centros de fotocopiado, Librería Giorgio, se expande hacia el área de abastecimiento incorporar a sus servicios todo lo relacionado al área de escritorio y oficina.
+  .title{
+    font-size: 20px;
+    font-weight: bold;
+    text-align: center;
+  }
+</style>
+<div class="container Somos">
+  <div class="row">
+    <div class="title">
+    <p>¿Quiénes Somos?</p>
+    </div>
+    <p>
+    Librería Giorgio es una empresa dedicada a la venta de suministros de oficina, artículos escolares, juguetería, y mucho más... Contamos con instalaciones para la venta de nuestros productos por volumen a empresas, ofreciendo un servicio de despacho completamente gratis. Asímismo disponemos de salas de ventas para atención de público en general, con estacionamientos propios para nuestros clientes. Durante años nos hemos dedicado a ser especialistas en Escolares, ofreciendo todo lo necesario para la Lista Escolar con los mejores precios del mercado. Trabajamos cada día para atender las necesidades y demandas de nuestros clientes, ampliando nuestro stock de productos y marcas asociadas para lograr satisfacción y fidelidad.
+    </p>
 
-Objetivo
-Conquistar la preferencia de los clientes mediante la entrega de un servicio eficiente y confiable, transformándonos en el mejor aliado como proveedor de suministros de oficina y artículos escolares.
+    <div class="title">
+    <p>Nuestra historia</p>
+    </div>
+    <p>Librería Giorgio es fundada el 1º de Septiembre del año 1992 en la ciudad de Concepción, inicialmente como un centro de fotocopiado ubicado en calle O`Higgins 734 local 14, Concepción. Considerando las necesidades de suministros e insumos para los centros de fotocopiado, Librería Giorgio, se expande hacia el área de abastecimiento incorporar a sus servicios todo lo relacionado al área de escritorio y oficina.
+    </p>
+    <div class="title">
+    <p>Objetivo</p>
+    </div>
+    <p>Conquistar la preferencia de los clientes mediante la entrega de un servicio eficiente y confiable, transformándonos en el mejor aliado como proveedor de suministros de oficina y artículos escolares.
+    </p>
+    <div class="title">
+    <p>Librería Giorgio</p>
+    </div>
 
-Librería Giorgio
-Actualmente estamos posicionados en el mercado gracias a la preferencia y fidelidad de nuestros clientes. Contamos con nuestra modalidad de venta Autoservicio y Asistido.
+    <p>Actualmente estamos posicionados en el mercado gracias a la preferencia y fidelidad de nuestros clientes. Contamos con nuestra modalidad de venta Autoservicio y Asistido.</p>
+  </div>
+</div>
